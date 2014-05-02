acdfc06af99c67a0821545a9e892a6d7e2fa457c
HybridRbt <littlejack0120@gmail.com>
2014-05-02 12:51:22 -0500

    Mod from the previous sample code and current

--- ps2-2.py
+++ ps2-2.py
@@ -18,16 +18,24 @@ def square_root(x, eps = 0.00001):
     y = math.sqrt(x)
     assert abs(square(y) - x) <= eps
     return y
-    
-def square(x):
-    return x * x
+
+# def square(x):
+#     return x * x
+
+def double(x):
+    return abs(20 * x) + 10
 
 # The Range class tracks the types and value ranges for a single variable.
 class Range:
+    # def __init__(self):
+    #     self.min = None  # Minimum value seen
+    #     self.max = None  # Maximum value seen
     def __init__(self):
         self.min = None  # Minimum value seen
         self.max = None  # Maximum value seen
-    
+        self.type = None  # Type of variable
+        self.set = set()  # Set of values taken
+
     # Invoke this for every value
     def track(self, value):
         # YOUR CODE
@@ -42,8 +50,11 @@ class Range:
         else:
             self.max = max(self.max, value)
             
-    def __repr__(self):
-        return repr(self.min) + ".." + repr(self.max)
+    # def __repr__(self):
+    #     return repr(self.min) + ".." + repr(self.max)
+
+        def __repr__(self):
+            repr(self.type) + " " + repr(self.min) + ".." + repr(self.max)+ " " + repr(self.set)
 
 
 # The Invariants class tracks all Ranges for all variables seen.
@@ -67,23 +78,43 @@ class Invariants:
             for each_item in frame.f_locals:
                 self.vars[frame.f_code.co_name][event][each_item].track(frame.f_locals[each_item])
 
+    # def __repr__(self):
+    #     # Return the tracked invariants
+    #     s = ""
+    #     for function, events in self.vars.iteritems():
+    #         for event, vars in events.iteritems():
+    #             s += event + " " + function + ":\n"
+    #             # continue
+    #
+    #             for var, range in vars.iteritems():
+    #                 s += "    assert "
+    #                 if range.min == range.max:
+    #                     s += var + " == " + repr(range.min)
+    #                 else:
+    #                     s += repr(range.min) + " <= " + var + " <= " + repr(range.max)
+    #                 s += "\n"
+    #
+    #     return s
     def __repr__(self):
         # Return the tracked invariants
         s = ""
         for function, events in self.vars.iteritems():
             for event, vars in events.iteritems():
                 s += event + " " + function + ":\n"
-                # continue
-                
+
                 for var, range in vars.iteritems():
+                    s += "    assert isinstance(" + var + # YOUR CODE
                     s += "    assert "
                     if range.min == range.max:
                         s += var + " == " + repr(range.min)
                     else:
                         s += repr(range.min) + " <= " + var + " <= " + repr(range.max)
                     s += "\n"
-                
+                    # ADD HERE RELATIONS BETWEEN VARIABLES
+                    # RELATIONS SHOULD BE ONE OF: ==, <=, >=
+                    s += "    assert " + var + " >= " + var2 + "\n"
         return s
+
 
 invariants = Invariants()
     
@@ -91,13 +122,31 @@ def traceit(frame, event, arg):
     invariants.track(frame, event, arg)
     return traceit
 
+# sys.settrace(traceit)
+# # Tester. Increase the range for more precise results when running locally
+# eps = 0.000001
+# for i in range(1, 1000):
+#     r = int(random.random() * 1000)  # An integer value between 0 and 999.99
+#     z = square_root(r, eps)
+#     z = square(z)
+# sys.settrace(None)
+# print invariants
+
 sys.settrace(traceit)
 # Tester. Increase the range for more precise results when running locally
 eps = 0.000001
-for i in range(1, 1000):
-    r = int(random.random() * 1000)  # An integer value between 0 and 999.99
-    z = square_root(r, eps)
-    z = square(z)
+test_vars = [34.6363, 9.348, -293438.402]
+for i in test_vars:
+#for i in range(1, 10):
+    z = double(i)
 sys.settrace(None)
 print invariants
 
+# Example sample of a correct output:
+"""
+return double:
+    assert isinstance(x, type(-293438.402))
+    assert x in set([9.348, -293438.402, 34.6363])
+    assert -293438.402 <= x <= 34.6363
+    assert x <= ret
+"""
